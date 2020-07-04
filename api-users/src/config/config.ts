dd258026c07fe69e2ef69945a92e22d341a5fc5d
Fernando Dávalos <fernando.davalos@freudenberg-it.com>
2020-07-03 19:17:07 -0500

    feat: Added Backend Users microservice

---
+++ api-users/src/config/config.ts
@@ -0,0 +1,21 @@
+import { config } from 'dotenv'
+
+if(process.env.NODE_ENV !== 'production'){
+    config();
+}
+
+export const configuration = {
+    'username': process.env.POSTGRES_USERNAME,
+    'password': process.env.POSTGRES_PASSWORD,
+    'database': process.env.POSTGRES_DATABASE,
+    'host': process.env.POSTGRES_HOST,
+    'dialect': 'postgres',
+    'aws_region': process.env.AWS_REGION,
+    'aws_profile': process.env.AWS_PROFILE,
+    'aws_media_bucket': process.env.AWS_BUCKET,
+    'url': process.env.URL,
+    'jwt': {
+      'secret': process.env.JWT_SECRET,
+    },
+  };
+  
