6b5bda12d8db6c55ff8aee08be5b064c7f285710
Fernando Dávalos <fernando.davalos@freudenberg-it.com>
2020-07-05 14:19:07 -0500

    fix: Modify cors in api-users

--- api-users/src/server.ts
+++ api-users/src/server.ts
@@ -18,15 +18,12 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
 
   app.use(bodyParser.json());
 
-  app.use(cors({
-    allowedHeaders: [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization',
-    ],
-    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: configuration.url,
-  }));
+  //CORS Should be restricted
+  app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+      next();
+  });
 
   app.use('/api/v0/', IndexRouter);
 
