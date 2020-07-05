165b3f609c23de9b1a14bc88728ebd24c5efdc33
Fernando Dávalos <fernando.davalos@freudenberg-it.com>
2020-07-05 02:29:07 -0500

    fix: change api endpoint to frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a7c4308446691414a963ae75e5fea4eb-1103753033.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
